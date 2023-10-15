1339ff9ac2acc7d4d354f7325741197d0d904509
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-15 17:04:53 -0400

    ripple animation when swithcing

--- HikeTrail/Views/HikeView.swift
+++ HikeTrail/Views/HikeView.swift
@@ -7,9 +7,18 @@ A view displaying information about a hike, including an elevation graph.
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        .asymmetric(
+            insertion: .move(edge: .trailing).combined(with: .opacity),
+            removal: .scale.combined(with: .opacity)
+        )
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
-    @State private var showDetail = false
+    @State private var showDetail = true
 
     var body: some View {
         VStack {
@@ -37,12 +46,13 @@ struct HikeView: View {
 //                        .animation(nil, value: showDetail)
                         .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
-//                        .animation(.spring(), value: showDetail)
+//                        .animation(.moveAndFade)
                 }
             }
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.slide)
             }
         }
     }
