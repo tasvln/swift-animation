443483766b0381894bf59be4d0f1e2515f3678f3
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-15 15:39:57 -0400

    Initial Commit

---
+++ HikeTrail/HikeTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeTrailApp.swift
+//  HikeTrail
+//
+//  Created by Temitope Adebayo on 2023-10-15.
+//
+
+import SwiftUI
+
+@main
+struct HikeTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
